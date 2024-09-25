fae36652f24b8c870f735c01e77ffc16e365c751
Sebastian <sebastian.danielsson@proton.me>
2024-09-26 01:51:00 +0200

    Switch to ts and ESM configs

---
+++ .prettierrc.mjs
@@ -0,0 +1,15 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+	plugins: ["prettier-plugin-tailwindcss"],
+	trailingComma: "es5",
+	tabWidth: 2,
+	useTabs: true,
+	singleQuote: false,
+	semi: true,
+	bracketSameLine: false,
+	bracketSpacing: true,
+	jsxSingleQuote: false,
+	quoteProps: "as-needed",
+	endOfLine: "lf",
+};
