1582c7420f1a3440315ae2ecf757298964fc0a7d
Timothy Jaeryang Baek <tim@openwebui.com>
2025-03-26 21:44:12 -0700

    refac

--- src/components/TopBanners.tsx
+++ src/components/TopBanners.tsx
@@ -3,17 +3,17 @@ import { TopBanner } from "@site/src/components/Sponsors/TopBanner";
 export const TopBanners = () => {
 	const items = [
 		{
-			imgSrc: "/ads/pipelines-banner.png",
-			mobileImgSrc: "/ads/pipelines-banner-mobile.png",
-			url: "https://github.com/open-webui/pipelines",
-			name: "Open WebUI Pipelines",
+			imgSrc: "/ads/openwebui-banner.png",
+			mobileImgSrc: "/ads/openwebui-banner-mobile.png",
+			url: "https://openwebui.com",
+			name: "Open WebUI Community",
 			description:
-				"Pipelines: Versatile, UI-Agnostic OpenAI-Compatible Plugin Framework",
+				"Join our growing community! Sign up to gain access to exclusive models, tools, and functions.",
 		},
 		{
 			imgSrc: "/ads/placeholder.png",
 			mobileImgSrc: "/ads/placeholder-mobile.png",
-			url: "https://github.com/sponsors/tjbck",
+			url: "mailto:sales@openwebui.com?subject=Sponsorship Inquiry: Open WebUI",
 			name: "Open WebUI",
 			description:
 				"The top banner spot is reserved for only two Enterprise sponsors on a first-come, first-served basis",
