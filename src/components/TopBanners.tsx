b3b2e2fe5bfa6ccb61fb41d429cf811861964eeb
Timothy Jaeryang Baek <tim@openwebui.com>
2025-03-27 19:38:12 -0700

    Update TopBanners.tsx

--- src/components/TopBanners.tsx
+++ src/components/TopBanners.tsx
@@ -3,16 +3,16 @@ import { TopBanner } from "@site/src/components/Sponsors/TopBanner";
 export const TopBanners = () => {
 	const items = [
 		{
-			imgSrc: "/ads/openwebui-banner.png",
-			mobileImgSrc: "/ads/openwebui-banner-mobile.png",
+			imgSrc: "/sponsors/banners/openwebui-banner.png",
+			mobileImgSrc: "/sponsors/banners/openwebui-banner-mobile.png",
 			url: "https://openwebui.com",
 			name: "Open WebUI Community",
 			description:
 				"Join our growing community! Sign up to gain access to exclusive models, tools, and functions.",
 		},
 		{
-			imgSrc: "/ads/placeholder.png",
-			mobileImgSrc: "/ads/placeholder-mobile.png",
+			imgSrc: "/sponsors/banners/placeholder.png",
+			mobileImgSrc: "/sponsors/banners/placeholder-mobile.png",
 			url: "mailto:sales@openwebui.com?subject=Sponsorship Inquiry: Open WebUI",
 			name: "Open WebUI",
 			description:
