6d4ca1c6c9ef6e9bd7c6b6a4434a5d4c615b950b
Timothy Jaeryang Baek <tim@openwebui.com>
2025-03-27 20:54:52 -0700

    refac

--- src/components/SponsorList.tsx
+++ src/components/SponsorList.tsx
@@ -14,8 +14,28 @@ export const SponsorList = () => {
 	return (
 
 		<div>
-
 			{/* <div className="mb-4">
+				<div className="font-semibold text-xl text-black dark:text-white ">Diamond</div>
+				<hr className="mt-1 mb-3 border-gray-200 dark:border-gray-700" />
+
+
+				<div className="flex flex-wrap items-start justify-start gap-5">
+					
+				</div>
+			</div>		
+
+
+			<div className="mb-4">
+				<div className="font-semibold text-xl text-black dark:text-white ">Sapphire</div>
+				<hr className="mt-1 mb-3 border-gray-200 dark:border-gray-700" />
+
+
+				<div className="flex flex-wrap items-start justify-start gap-5">
+					
+				</div>
+			</div>					
+
+			<div className="mb-4">
 				<div className="font-semibold text-xl text-black dark:text-white ">Topaz</div>
 				<hr className="mt-1 mb-3 border-gray-200 dark:border-gray-700" />
 
