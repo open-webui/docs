5a13947c88e29e45b3a5b1ff364f9b9f541a21a8
Timothy Jaeryang Baek <tim@openwebui.com>
2025-07-05 21:05:12 +0400

    refac

--- src/components/TopBanners.tsx
+++ src/components/TopBanners.tsx
@@ -23,23 +23,6 @@ export const TopBanners = ({
 			name: "n8n",
 			description: "Does your interface have a backend yet? Try n8n",
 		},
-
-		{
-			imgSrc: "/sponsors/banners/warp-banner.png",
-			mobileImgSrc: "/sponsors/banners/warp-banner-mobile.png",
-			url: "https://warp.dev/open-webui",
-			name: "Warp",
-			description: "The intelligent terminal for developers",
-		},
-
-		{
-			imgSrc: "/sponsors/banners/warp-banner.png",
-			mobileImgSrc: "/sponsors/banners/warp-banner-mobile.png",
-			url: "https://warp.dev/open-webui",
-			name: "Warp",
-			description: "The intelligent terminal for developers",
-		},
-
 		{
 			imgSrc: "/sponsors/banners/tailscale-banner.png",
 			mobileImgSrc: "/sponsors/banners/tailscale-banner-mobile.png",
