051a5b156171e9297870fa6655d53c60ef9e9ed6
DrMelone <27028174+Classic298@users.noreply.github.com>
2025-12-20 18:42:37 +0100

    fix ci

--- src/components/Sponsors/TopBanner.tsx
+++ src/components/Sponsors/TopBanner.tsx
@@ -51,7 +51,9 @@ export const TopBanner = ({
 
 				{description && (
 					<div className="mt-1 line-clamp-1 text-right text-xs font-semibold text-gray-600 dark:text-gray-300">
-						<div dangerouslySetInnerHTML={{ __html: marked(item.description) }} />
+						<div
+							dangerouslySetInnerHTML={{ __html: marked(item.description) }}
+						/>
 					</div>
 				)}
 			</div>
