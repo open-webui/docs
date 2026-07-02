aa0ffb45136c4275f295c540a6d9b2e14d417277
Timothy Jaeryang Baek <tim@openwebui.com>
2026-07-02 15:40:08 -0500

    refac

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
 	favicon: "images/favicon.png",
 
 	// Set the production url of your site here
-	url: "https://openwebui.com",
+	url: "https://docs.openwebui.com",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: "/",
